a9f835e5bf6013ca9905625e443fc551efaf8cb5
Hetelekides, Stergios <hetelek@gmail.com>
2017-03-15 09:41:32 -0700

    Add page to playground

--- Neural Network.playground/Pages/Main.xcplaygroundpage/Contents.swift
+++ Neural Network.playground/Pages/Main.xcplaygroundpage/Contents.swift
@@ -1,12 +1,13 @@
 import Foundation
-import XCPlayground
+import UIKit
+import PlaygroundSupport
 
 let net = Network(inputs: 2, structure: [2, 1])!
 let inputs: [[Double]] = [[1, 1], [1, 0], [0, 1], [0, 0]]
 let outputs: [[Double]] = [[1], [1], [1], [0]]
 
 var cost: Double?
-for _ in 0...1000 {
+for _ in 0...100 {
     for index in 0..<inputs.count {
         print("got: \(net.feed(inputs: inputs[index])), expected: \(outputs[index])")
     }
@@ -15,3 +16,5 @@ for _ in 0...1000 {
     net.batchTrain(batchInputs: inputs, batchExpectedOutputs: outputs, η: 2)
     print("-")
 }
+
+cost
