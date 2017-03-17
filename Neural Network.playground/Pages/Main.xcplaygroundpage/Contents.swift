d6a710c27d7c749f197f954a9e70352b9b3941c0
Hetelekides, Stergios <hetelek@gmail.com>
2017-03-17 11:45:58 -0700

    Fix backpropagation to be able to handle XOR

--- Neural Network.playground/Pages/Main.xcplaygroundpage/Contents.swift
+++ Neural Network.playground/Pages/Main.xcplaygroundpage/Contents.swift
@@ -4,17 +4,18 @@ import PlaygroundSupport
 
 let net = Network(inputs: 2, structure: [2, 1])!
 let inputs: [[Double]] = [[1, 1], [1, 0], [0, 1], [0, 0]]
-let outputs: [[Double]] = [[1], [1], [1], [0]]
+let outputs: [[Double]] = [[0], [1], [1], [0]]
 
 var cost: Double?
-for _ in 0...100 {
+for _ in 0...100000 {
+    var total: Double = 0
     for index in 0..<inputs.count {
-        print("got: \(net.feed(inputs: inputs[index])), expected: \(outputs[index])")
+        let got = net.feed(inputs: inputs[index])[0][0]
+        total += 0.5 * pow(got - outputs[index][0], 2)
+        print("got: \(got), expected: \(outputs[index])")
     }
     
-    cost = net.cost(inputs: inputs[0], expectedOutputs: outputs[0])
-    net.batchTrain(batchInputs: inputs, batchExpectedOutputs: outputs, η: 2)
-    print("-")
+    cost = total / Double(inputs.count)
+    net.batchTrain(batchInputs: inputs, batchExpectedOutputs: outputs, η: 0.5)
+    print("")
 }
-
-cost
--- Neural Network.playground/Sources/Network.swift
+++ Neural Network.playground/Sources/Network.swift
@@ -4,17 +4,25 @@ public class Network {
     private(set) var layers: [Matrix] = []
     private(set) var biases: [Matrix] = []
     
+    var σ: (Double) -> Double = { x in
+        return 1.0 / (1 + pow(M_E, -x))
+    }
+    var σPrime: (Double) -> Double = { x in
+        let sigmoidValue = 1.0 / (1 + pow(M_E, -x))
+        return sigmoidValue * (1 - sigmoidValue)
+    }
+    
     public init?(inputs: Int, structure: [Int]) {
         guard let first = structure.first else {
             return nil
         }
         
-        layers.append(Matrix(rows: inputs, columns: first))
-        biases.append(Matrix(rows: 1, columns: first, repeating: 0.1))
+        layers.append(Matrix(rows: first, columns: inputs))
+        biases.append(Matrix(rows: first, columns: 1, repeating: 0.1))
         
         for hiddenNeurons in structure.dropFirst() {
-            layers.append(Matrix(rows: layers.last!.columns, columns: hiddenNeurons))
-            biases.append(Matrix(rows: 1, columns: hiddenNeurons, repeating: 0.1))
+            layers.append(Matrix(rows: hiddenNeurons, columns: layers.last!.rows))
+            biases.append(Matrix(rows: hiddenNeurons, columns: 1, repeating: 0.1))
         }
     }
     
@@ -23,87 +31,102 @@ public class Network {
         return a.last!
     }
     
-    public func cost(inputs: [Double], expectedOutputs: [Double]) -> Double {
-        let expectedOutput = Matrix(elements: [expectedOutputs])
-        let (_, a) = internalFeed(inputs: inputs)
-        let difference = (a.last! - expectedOutput) ^ 2
-        return (1.0 / (2.0 * 1)) * difference.sum()
-    }
-    
     public func batchTrain(batchInputs: [[Double]], batchExpectedOutputs: [[Double]], η: Double) {
-        var derivativeSums: [Matrix] = []
-        var errorSums: [Matrix] = []
-
-        // calculate errors and derivatives for each sample
-        for batchIndex in 0..<batchInputs.count {
-            let inputs = batchInputs[batchIndex]
-            let expectedOutput: Matrix = [batchExpectedOutputs[batchIndex]]
-            let (allActivations, allSigmoidActivations) = internalFeed(inputs: inputs)
+        var weightGradients: [Matrix]!
+        var biasGradients: [Matrix]!
+        
+        for sampleIndex in 0..<batchInputs.count {
+            let inputs = batchInputs[sampleIndex]
+            let expectedOutputs = batchExpectedOutputs[sampleIndex]
             
-            let errors = calculateErrors(allActivations: allActivations, allSigmoidActivations: allSigmoidActivations, expectedOutput: expectedOutput)
-            for index in 0..<errors.count {
-                let layerErrors = errors[index]
-                
-                let derivatives = layerErrors.transpose().elementwiseProduct(matrix: allActivations[index])
-                if batchIndex == 0 {
-                    derivativeSums.append(derivatives)
-                    errorSums.append(layerErrors)
+            let (sampleWeightGradients, sampleBiasGradients) = calculateGradients(inputs: inputs, expectedOutputs: expectedOutputs)
+            if weightGradients != nil {
+                // add the new gradients to the existing ones (later used to take gradient avergae)
+                for layerIndex in 0..<sampleWeightGradients.count {
+                    weightGradients[layerIndex] = weightGradients[layerIndex] + sampleWeightGradients[layerIndex]
+                    biasGradients[layerIndex] = biasGradients[layerIndex] + sampleBiasGradients[layerIndex]
                 }
-                else {
-                    derivativeSums[index] = derivativeSums[index] + derivatives
-                    errorSums[index] = errorSums[index] + layerErrors
-                }
+            }
+            else {
+                weightGradients = sampleWeightGradients
+                biasGradients = sampleBiasGradients
             }
         }
         
-        // update the weights
-        for layerIndex in 0..<derivativeSums.count {
-            let layer = layers[layerIndex]
-            let derivativeAverage = derivativeSums[layerIndex] / Double(derivativeSums.count)
-            let errorAverage = errorSums[layerIndex] / Double(derivativeSums.count)
+        let batchSize = Double(batchInputs.count)
+        for layerIndex in 0..<layers.count {
+            let layerWeightGradients = weightGradients[layerIndex] / batchSize
+            let layerBiasGradients = biasGradients[layerIndex] / batchSize
             
-            for row in 0..<layer.rows {
-                for col in 0..<layer.columns {
-                    layer[row][col] = layer[row][col] - (derivativeAverage[0][col] * η)
-                }
-            }
+            // update weights
+            layers[layerIndex] = layers[layerIndex] - (layerWeightGradients * η)
             
-            biases[layerIndex] = biases[layerIndex] - (errorAverage.transpose() * η)
+            // update biases
+            biases[layerIndex] = biases[layerIndex] - (layerBiasGradients * η)
         }
     }
     
     public func train(inputs: [Double], expectedOutputs: [Double], η: Double) {
-        let expectedOutput = Matrix(elements: [expectedOutputs])
-        let (allActivations, allSigmoidActivations) = internalFeed(inputs: inputs)
+        let (weightGradients, biasGradients) = calculateGradients(inputs: inputs, expectedOutputs: expectedOutputs)
+        print(weightGradients)
         
-        let errors = calculateErrors(allActivations: allActivations, allSigmoidActivations: allSigmoidActivations, expectedOutput: expectedOutput)
-        for index in 0..<errors.count {
-            let layerErrors = errors[index]
-            let layer = layers[index]
+        for layerIndex in 0..<layers.count {
+            // update weights
+            layers[layerIndex] = layers[layerIndex] - (weightGradients[layerIndex] * η)
             
-            let derivatives = layerErrors.transpose().elementwiseProduct(matrix: allActivations[index])
-            for row in 0..<layer.rows {
-                for col in 0..<layer.columns {
-                    layer[row][col] = layer[row][col] - (derivatives[0][col] * η)
+            // update biases
+            biases[layerIndex] = biases[layerIndex] - (biasGradients[layerIndex] * η)
+        }
+    }
+    
+    private func calculateGradients(inputs: [Double], expectedOutputs: [Double]) -> (weightGradients: [Matrix], biasGradients: [Matrix]) {
+        // our return variables
+        var allWeightGradients: [Matrix] = []
+        var allBiasGradients: [Matrix] = []
+        
+        // forward pass
+        let expectedOutput = Matrix(elements: [expectedOutputs]).transpose()
+        var (allActivations, allSigmoidActivations) = internalFeed(inputs: inputs)
+        
+        // backpropagate
+        let errors = calculateErrors(allActivations: allActivations, allSigmoidActivations: allSigmoidActivations, expectedOutput: expectedOutput)
+        
+        allSigmoidActivations.insert(Matrix(elements: [inputs]).transpose(), at: 0)
+        
+        for (index, error) in errors.enumerated() {
+            // calculate weight gradients
+            let layer = layers[index]
+            let weightGradients = Matrix(rows: layer.rows, columns: layer.columns, repeating: 0)
+            for row in 0..<weightGradients.rows {
+                for col in 0..<weightGradients.columns {
+                    weightGradients[row][col] = error[row][0] * allSigmoidActivations[index][col][0]
                 }
             }
-
-            biases[index] = biases[index] - (layerErrors.transpose() * η)
+            
+            // bias gradients are exactly equal to errors
+            let biasGradients = error
+            
+            // add to array
+            allWeightGradients.append(weightGradients)
+            allBiasGradients.append(biasGradients)
         }
+        
+        return (allWeightGradients, allBiasGradients)
     }
     
     private func calculateErrors(allActivations: [Matrix], allSigmoidActivations: [Matrix], expectedOutput: Matrix) -> [Matrix] {
         // cost error
-        let outputSensitivity = allActivations.last!.elementMap(transform: Network.sigmoidDerivative)
-        let outputError = (allSigmoidActivations.last! - expectedOutput).elementwiseProduct(matrix: outputSensitivity).transpose()
+        let activationDerivative = allActivations.last!.elementMap(transform: self.σPrime)
+        let outputError = (allSigmoidActivations.last! - expectedOutput).elementwiseProduct(matrix: activationDerivative)
         
         // layer errors
         var errors = [outputError]
-        for index in (1..<layers.count).reversed() {
+        for index in (0..<(layers.count - 1)).reversed() {
             let previousError = errors.last!
-            let layer = layers[index]
-            let activation = allActivations[index - 1].transpose()
-            let activationSigmoidPrime = activation.elementMap(transform: Network.sigmoidDerivative)
+            let layer = layers[index + 1].transpose()
+            
+            let activation = allActivations[index]
+            let activationSigmoidPrime = activation.elementMap(transform: self.σPrime)
             
             let newError = (layer * previousError).elementwiseProduct(matrix: activationSigmoidPrime)
             errors.append(newError)
@@ -116,27 +139,16 @@ public class Network {
         var z: [Matrix] = []
         var a: [Matrix] = []
         
-        var output = Matrix(elements: [inputs])
+        var output = Matrix(elements: [inputs]).transpose()
         
         for (index, layer) in layers.enumerated() {
-            output = (output * layer) + biases[index]
+            output = (layer * output) + biases[index]
             z.append(output)
             
-            output = output.elementMap(transform: Network.sigmoid)
+            output = output.elementMap(transform: self.σ)
             a.append(output)
         }
         
         return (z, a)
-    }
-    
-    
-    // MARK: - Math functions
-    private static func sigmoid(input: Double) -> Double {
-        return 1.0 / (1 + pow(M_E, -input))
-    }
-    
-    private static func sigmoidDerivative(input: Double) -> Double {
-        let sigmoidValue = sigmoid(input: input)
-        return sigmoidValue * (1 - sigmoidValue)
     }
 }
