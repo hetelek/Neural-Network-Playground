050e6cc987bb4726af2b737b436df060490861ad
Hetelekides, Stergios <hetelek@gmail.com>
2017-03-26 22:02:15 -0700

    Make view controller for MNIST

--- Neural Network.playground/Pages/DigitRecognition.xcplaygroundpage/Contents.swift
+++ Neural Network.playground/Pages/DigitRecognition.xcplaygroundpage/Contents.swift
@@ -1,19 +1,6 @@
-import Foundation
 import PlaygroundSupport
 import UIKit
 
-let url = Bundle.main.url(forResource: "t10k-images-idx3-ubyte", withExtension: nil)!
-let mnist = MNIST(url: url)!
-
-let (pixelIntensities, image) = mnist.getImage()
-let imageView = UIImageView(image: image)
-PlaygroundPage.current.liveView = imageView
-
-let url2 = Bundle.main.url(forResource: "mnist-network-weights", withExtension: nil)!
-
-let network = Network(url: url2)
-let results = network.feed(inputs: pixelIntensities)
-
-
-let (_, value, _) = results.max()
-print("I think this is a \(value).")
+let mnistController = MNISTViewController()
+let navigationController = UINavigationController(rootViewController: mnistController)
+PlaygroundPage.current.liveView = navigationController
---
+++ Neural Network.playground/Pages/DigitRecognition.xcplaygroundpage/Sources/MNISTViewController.swift
@@ -0,0 +1,76 @@
+import UIKit
+
+public class MNISTViewController: UIViewController {
+    // MARK: - Private properties
+    private let mnist: MNIST = {
+        let url = Bundle.main.url(forResource: "t10k-images-idx3-ubyte", withExtension: nil)!
+        return MNIST(url: url)!
+    }()
+    private let mnistNetwork: Network = {
+        let url2 = Bundle.main.url(forResource: "mnist-network-weights", withExtension: nil)!
+        return Network(url: url2)
+    }()
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    private let predictionLabel: UILabel = {
+        let predictionLabel = UILabel()
+        predictionLabel.translatesAutoresizingMaskIntoConstraints = false
+        predictionLabel.textAlignment = .center
+        return predictionLabel
+    }()
+    
+    
+    // MARK: - View Controller Lifecycle
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // main view setup
+        title = "Handwritting Prediction View"
+        view.backgroundColor = .white
+        
+        // image view setup
+        view.addSubview(imageView)
+        NSLayoutConstraint.activate([
+            imageView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            imageView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor),
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
+        // prediction label setup
+        view.addSubview(predictionLabel)
+        NSLayoutConstraint.activate([
+            predictionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            predictionLabel.bottomAnchor.constraint(equalTo: imageView.topAnchor, constant: -10)
+        ])
+        
+        // next image button setup
+        let button = UIButton(type: .system)
+        button.setTitle("Next Image", for: .normal)
+        button.addTarget(self, action: #selector(makeNewPrediction), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10)
+        ])
+        
+        // make the first prediction
+        makeNewPrediction()
+    }
+    
+    
+    // MARK: - Button actions
+    @objc private func makeNewPrediction() {
+        // get an image and it's pixel intensities using MNIST object
+        let (pixelIntensities, image) = mnist.getImage()
+        imageView.image = image
+        
+        // get network's prediction
+        let (_, output, _) = mnistNetwork.feed(inputs: pixelIntensities).max()
+        predictionLabel.text = "I think this is the number \(output)."
+    }
+}
