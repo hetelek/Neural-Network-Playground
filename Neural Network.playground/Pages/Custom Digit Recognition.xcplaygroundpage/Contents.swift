274f4f6ab4a906e2d8571b2242fcffb7ac283c78
Hetelekides, Stergios <hetelek@gmail.com>
2017-03-28 10:40:23 -0700

    Markup updates

--- Neural Network.playground/Pages/Custom Digit Recognition.xcplaygroundpage/Contents.swift
+++ Neural Network.playground/Pages/Custom Digit Recognition.xcplaygroundpage/Contents.swift
@@ -4,7 +4,7 @@ import PlaygroundSupport
  [Back to Handwritten Digit Recognition](@previous)
  
  ## Drawing Digits
- Here you can try drawing your own digits and seeing how the network performs. You can see that it's not perfect, but works quite well for being so simple.
+ Here you can try drawing your own digits and see how the network performs. You can see that it's not perfect, but works quite well for being so simple.
  
  Keep in mind that the network is making guesses based on its training data. The images on the [previous page](@previous) give a good representation of the training data. The closer your drawings look to those, the better the network's guesses will get.
  
